7670af6d5229890c0af27cb279b02ee1728d9297
Pavel Korobov <korobovpvl@gmail.com>
2025-01-22 13:22:26 +0300

    pyinstaller

--- setup.py
+++
@@ -1,7 +0,0 @@
-import py2exe
-
-version = '2.x'
-path = f'../../release/karma_bot/{version}/dist'
-py2exe.freeze(console=[{'script': 'main.py'}], 
-              options={'packages': ['tools', 'handlers', 'charset_normalizer'],
-                       'dist_dir': path})
