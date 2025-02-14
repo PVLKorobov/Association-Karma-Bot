85bda6b5e7545444bca83606ae5aef8b0008af39
PVL-NBook <anakashalban@gmail.com>
2025-02-14 16:40:21 +0300

    new handlers directory structure

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from aiogram import Dispatcher
 from tools.Bot import botInstance
 from tools import bot_commands
 
-from handlers import default_commands, group_commands, messages
+from handlers import default_commands
+from handlers.group_monitor import group_commands, group_messages
+from handlers.private_group_registration import *
 
 
 async def main() -> None:
     dp = Dispatcher()
-    dp.include_routers(group_commands.router, default_commands.router, messages.router)
+    dp.include_routers(group_commands.router, default_commands.router, group_messages.router)
     await bot_commands.check_commands()
     await dp.start_polling(botInstance)
 
